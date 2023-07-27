a93013410a81df4aaab200ac682c95e4d54f4c13
Chad Smith <chad@koovisa.com>
2023-07-27 20:56:50 +0100

    Final working program

--- src/FizzBuzz/NumberToStringTranslatorLoader.cs
+++ src/FizzBuzz/NumberToStringTranslatorLoader.cs
@@ -1,12 +1,27 @@
 namespace FizzBuzz;
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Shared;
 
 public class NumberToStringTranslatorLoader {
     public IEnumerable<INumberToStringTranslator> LoadAll() {
         var translators = new List<INumberToStringTranslator>();
 
-        return translators;
+        var path = Path.Combine(
+            Assembly.GetExecutingAssembly().Location,
+            "../../../../..", // from bin/debug/net6.0 folder to the src folder
+            "NumberToStringTranslators/bin/debug/net6.0",
+            "NumberToStringTranslators.dll"
+        );
+
+        var translatorTypes = AppDomain.CurrentDomain.Load(File.ReadAllBytes(path))
+            .GetTypes()
+            .Where( t => !t.IsInterface && typeof(INumberToStringTranslator).IsAssignableFrom(t));
+
+        return translatorTypes.Select( t => (INumberToStringTranslator)Activator.CreateInstance(t)! );
     }
 }
